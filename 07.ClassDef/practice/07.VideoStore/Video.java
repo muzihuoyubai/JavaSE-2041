0cba6a0d77cb5f6fd07477ca15a75285523206f5
李熠 <liyi@banyuan.club>
2020-06-12 10:50:59 +0800

    初版

---
+++ 07.ClassDef/practice/07.VideoStore/Video.java
@@ -0,0 +1,20 @@
+class Video {
+
+    // - 片名
+    String title;
+
+    // - 是否被出租的标识
+    // 1 出租  0 未出租
+    // int isRent = 1;
+    // 出租 未出租
+    // String isRent = "出租";
+    // true 被出租  false 未被出租
+    boolean isRent;
+// - 用户的平均评分
+    double rating;
+
+    int ratingTimes;
+
+    // int[] ratingArr = new int[10]{-1};
+
+}
