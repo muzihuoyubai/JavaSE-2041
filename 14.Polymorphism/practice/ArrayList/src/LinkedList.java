3846329ca0e7a8a5ca7ff6de06c0870ce452df9e
李熠 <liyi@banyuan.club>
2020-06-23 10:55:52 +0800

    链表的实现

---
+++ 14.Polymorphism/practice/ArrayList/src/LinkedList.java
@@ -0,0 +1,73 @@
+public class LinkedList {
+
+  private Node head;
+
+  /**
+   * 更新数组指定下标的元素
+   *
+   * @param index   待更新的元素下标
+   * @param element 更新后的元素对象
+   * @return 被替换掉的旧对象
+   */
+  public Object set(int index, Object element) {
+
+  }
+
+  /**
+   * 查询指定位置下标的元素
+   *
+   * @param index 需要判断index是否合法
+   * @return 返回查找到的元素，找不到返回null
+   */
+  public Object get(int index) {
+  }
+
+  /**
+   * 清空集合内容
+   */
+  public void clear() {
+  }
+
+  /**
+   * 删除指定下标的元素
+   *
+   * @param index 元素下标
+   * @return 将删除的元素返回，如果下标不合理，返回null
+   */
+  public Object remove(int index) {
+  }
+
+  /**
+   * 查找并删除元素
+   *
+   * @param o 通过目标元素 equals 方法判断是否匹配，
+   *          需要判断o是否为null，如果传入null，则用== 进行比较
+   * @return
+   */
+  public boolean remove(Object o) {
+  }
+
+
+  /**
+   * 添加一个元素
+   *
+   * @param o
+   * @return 添加成功后返回true 失败返回false
+   */
+  public boolean add(Object o) {
+  }
+
+  /**
+   * @return 数组中没有元素，返回true
+   */
+  public boolean isEmpty() {
+  }
+
+  /**
+   * 返回数组中保存的元素个数
+   *
+   * @return
+   */
+  public int size() {
+  }
+}
