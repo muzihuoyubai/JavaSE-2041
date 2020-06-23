1fbfcafe7416f6fcc55e721687aebf5c09438d79
李熠 <liyi@banyuan.club>
2020-06-23 13:56:25 +0800

    ArrayList实现

--- 14.Polymorphism/practice/ArrayList/src/ArrayList.java
+++ 14.Polymorphism/practice/ArrayList/src/ArrayList.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
 
 public class ArrayList {
 
   private Object[] value;
+  private int count;
 
   public ArrayList(int capacity) {
     value = new Object[capacity];
@@ -19,7 +21,14 @@ public class ArrayList {
    * @return 被替换掉的旧对象
    */
   public Object set(int index, Object element) {
+    if (index < 0 || index >= count) {
+      System.err.println("index不合法");
+      return null;
+    }
 
+    Object rlt = value[index];
+    value[index] = element;
+    return rlt;
   }
 
   /**
@@ -29,12 +38,19 @@ public class ArrayList {
    * @return 返回查找到的元素，找不到返回null
    */
   public Object get(int index) {
+    if (index < 0 || index >= count) {
+      System.err.println("index不合法");
+      return null;
+    }
+
+    return value[index];
   }
 
   /**
    * 清空集合内容
    */
   public void clear() {
+    value = new Object[value.length];
   }
 
   /**
@@ -44,6 +60,16 @@ public class ArrayList {
    * @return 将删除的元素返回，如果下标不合理，返回null
    */
   public Object remove(int index) {
+    if (index < 0 || index >= count) {
+      System.err.println("index不合法");
+      return null;
+    }
+    Object rlt = value[index];
+    System.arraycopy(value, index + 1, value, index, count - index - 1);
+
+    value[count - 1] = null;
+    count--;
+    return rlt;
   }
 
   /**
@@ -54,6 +80,14 @@ public class ArrayList {
    * @return
    */
   public boolean remove(Object o) {
+    for (int i = 0; i < count; i++) {
+      if (Objects.equals(o, value[i])) {
+        remove(i);
+        return true;
+      }
+    }
+    return false;
+
   }
 
 
@@ -64,12 +98,23 @@ public class ArrayList {
    * @return 添加成功后返回true 失败返回false
    */
   public boolean add(Object o) {
+
+    // 如果数组满了进行扩容
+    if (count == value.length) {
+      Object[] newArr = new Object[value.length * 2];
+      System.arraycopy(value, 0, newArr, 0, count);
+      value = newArr;
+    }
+
+    value[count++] = o;
+    return true;
   }
 
   /**
    * @return 数组中没有元素，返回true
    */
   public boolean isEmpty() {
+    return count == 0;
   }
 
   /**
@@ -78,31 +123,39 @@ public class ArrayList {
    * @return
    */
   public int size() {
+    return count;
   }
 
 
   public static void main(String[] args) {
-    ArrayList arrayList = new ArrayList();
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
-    arrayList.add(2);
 
-    Object o = arrayList.get(1);
-    int a = (int) o;
+    ArrayList arrayList = new ArrayList(3);
+    arrayList.add(1);
+    arrayList.add(2);
+    arrayList.add(3);
+    arrayList.add(4);
+    arrayList.add(5);
+
+    for (int i = 0; i < arrayList.size(); i++) {
+      System.out.println(arrayList.get(i));
+    }
+
+    System.out.println(arrayList.isEmpty());
+
+    System.out.println(arrayList.remove(1));
+
+    System.out.println(arrayList.remove(Integer.valueOf(4)));
+
+    System.out.println("============");
+
+    for (int i = 0; i < arrayList.size(); i++) {
+      System.out.println(arrayList.get(i));
+    }
+    arrayList.set(0, 200);
+    System.out.println("+++++++++++++");
+    for (int i = 0; i < arrayList.size(); i++) {
+      System.out.println(arrayList.get(i));
+    }
+
   }
 }
