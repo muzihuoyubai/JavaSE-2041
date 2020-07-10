38ba0eb51bc1ee4fddaa1d19c93781672202d75f
李熠 <liyi@banyuan.club>
2020-07-10 11:14:09 +0800

    练习解析  cmd

--- 21.IO/practice/02.CaesarCipher/src/Decode.java
+++ 21.IO/practice/02.CaesarCipher/src/Decode.java
@@ -1,9 +1,17 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Decode {
 
-  public static void main(String[] args) {
+  // /Users/liyi/works/repos/banyuan/JavaSE-2041/21.IO/practice/02.CaesarCipher/alice.code /Users/liyi/works/repos/banyuan/JavaSE-2041/21.IO/practice/02.CaesarCipher/alice.code.decode
+  static String encodeFilePath = "/Users/liyi/works/repos/banyuan/JavaSE-2041/21.IO/practice/02.CaesarCipher/alice.code";
+  static String decodeFilePath = "/Users/liyi/works/repos/banyuan/JavaSE-2041/21.IO/practice/02.CaesarCipher/alice.code.decode";
+
+  public static void main(String[] args) throws IOException {
     BufferedReader inStream = null;
     BufferedWriter outStream = null;
 
@@ -16,6 +24,30 @@ public class Decode {
     //完成此部分代码，调用 caesarDecode 对传入的inputFilePath文件进行解密
     //将解密后的文本输出到 outputFilePath 文件中
     //尝试将alice.code进行解密
+    try {
+      inStream = new BufferedReader(new FileReader(inputFilePath));
+      outStream = new BufferedWriter(new FileWriter(outputFilePath));
+
+      String line = inStream.readLine();
+      while (line != null) {
+        char[] chars = line.toCharArray();
+        for (char aChar : chars) {
+          outStream.write(caesarDecode(aChar));
+        }
+        outStream.newLine();
+        line = inStream.readLine();
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } finally {
+      if (inStream != null) {
+        inStream.close();
+      }
+
+      if (outStream != null) {
+        outStream.close();
+      }
+    }
 
     System.out.println("解密成功！");
   }
--- 21.IO/practice/03.Cmd/src/club/banyuan/Main.java
+++ 21.IO/practice/03.Cmd/src/club/banyuan/Main.java
@@ -1,10 +1,61 @@
 package club.banyuan;
 
+import java.io.File;
+
 public class Main {
 
   public static void main(String[] args) {
-    for (String arg : args) {
-      System.out.println("参数：" + arg);
+
+    // ls
+    // ls 路径
+    // ls 路径 -l
+    // ls -l
+    // cp 路径1 路径2
+    // rm 路径
+    // cat 文件路径
+
+    try {
+      if (args.length < 1) {
+        throw new RuntimeException("参数不合法");
+      }
+
+      String cmd = args[0];
+      switch (cmd) {
+        case "ls":
+          ls(args);
+          break;
+        case "cp":
+          break;
+        case "cat":
+          break;
+        case "rm":
+          break;
+        default:
+          throw new RuntimeException("不认识的命令" + cmd);
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
+
+  private static void ls(String[] args) {
+    if (args.length == 1) {
+      File file = new File(".");
+      File[] list = file.listFiles();
+      if (list == null || list.length == 0) {
+        return;
+      }
+      int newLine = 1;
+      for (File s : list) {
+        if (newLine % 5 == 0) {
+          System.out.println();
+          newLine = 1;
+        }
+        System.out.print(s.getName() + "\t\t");
+        newLine++;
+      }
     }
   }
+
 }
